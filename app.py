8418110aa3a4683c5edfa0b55227fe7140eb8706
amceljhb025 <amceljhb025@student.wethinkcode.co.za>
2026-09-13 16:53:13 +0200

    streamlit starter code for webapp implementation

---
+++ app.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+import streamlit as st
+
+st.set_page_config(page_title="iBafana Bafana & the World Cup", layout="wide")
+st.title("Bafana Bafana's All Time Perfomance at the Fifa World Cup")
+st.markdown("Tracking South Africa's Football Team perfomance in the Fifa World Cup from their debut in 1998 onwards.")
+
+
+
+with st.bottom:
+    st.write("© 2026 by uamahlecele.")
